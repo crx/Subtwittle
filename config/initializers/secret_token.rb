804c27b72d807f20f88322d21139f88fa69de2ec
Chase DuBois <carchase@gmail.com>
2012-01-27 15:58:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subtwittle::Application.config.secret_token = '74b472d5d44bf841634e29663e6bf765e730318d62303934e2acfe28a045d5e3ba2111122eeff0dee10b392038a64a889a8d926fe7e734904fb0a1d6a733ed97'
---
+++ test/unit/helpers/tweetimage_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TweetimageHelperTest < ActionView::TestCase
+end
